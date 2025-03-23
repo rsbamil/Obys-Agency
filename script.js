c529aa4a53312c7ba92c2cfcc7f4eb5eefb0937f
Rohit Kumar <rsbamil66@gamil.com>
2025-03-23 23:17:47 +0530

    Made Loader Successfully

---
+++ script.js
@@ -0,0 +1,41 @@
+var tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(() => {
+      if (grow < 100) {
+        grow++;
+        h5timer.innerHTML = grow;
+      } else {
+        grow = 100;
+      }
+    }, 33);
+  },
+});
+tl.to(".line h2", {
+  animationName: "Anime",
+  opacity: 1,
+});
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 4,
+});
+tl.from("#page1", {
+  delay: 0.2,
+  y: 1600,
+  opacity: 0,
+  ease: Power4,
+  duration: 0.5,
+});
+tl.to("#loader", {
+  display: none,
+});
