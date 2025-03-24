b75ba37e4b16d7bde615651a030a8419e0f0a1af
Rohit Kumar <rsbamil66@gamil.com>
2025-03-24 21:31:11 +0530

    page4 complete

--- script.js
+++ script.js
@@ -1,54 +1,54 @@
-function loadingAnimation() {
-  var tl = gsap.timeline();
-  tl.from(".line h1", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5,
-  });
-  tl.from("#line1-part1", {
-    opacity: 0,
-    onStart: function () {
-      var h5timer = document.querySelector("#line1-part1 h5");
-      var grow = 0;
-      setInterval(() => {
-        if (grow < 100) {
-          grow++;
-          h5timer.innerHTML = grow;
-        } else {
-          grow = 100;
-        }
-      }, 33);
-    },
-  });
-  tl.to(".line h2", {
-    animationName: "Anime",
-    opacity: 1,
-  });
-  tl.to("#loader", {
-    opacity: 0,
-    duration: 0.2,
-    delay: 0,
-  });
-  tl.from("#page1", {
-    delay: 0.2,
-    y: 1600,
-    opacity: 0,
-    ease: Power4,
-    duration: 0.5,
-  });
-  tl.to("#loader", {
-    display: "none",
-  });
-  tl.from("#nav", {
-    opacity: 0,
-  });
-  tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1 ", {
-    y: 120,
-    stagger: 0.2,
-  });
-}
-loadingAnimation();
+// function loadingAnimation() {
+//   var tl = gsap.timeline();
+//   tl.from(".line h1", {
+//     y: 150,
+//     stagger: 0.25,
+//     duration: 0.6,
+//     delay: 0.5,
+//   });
+//   tl.from("#line1-part1", {
+//     opacity: 0,
+//     onStart: function () {
+//       var h5timer = document.querySelector("#line1-part1 h5");
+//       var grow = 0;
+//       setInterval(() => {
+//         if (grow < 100) {
+//           grow++;
+//           h5timer.innerHTML = grow;
+//         } else {
+//           grow = 100;
+//         }
+//       }, 33);
+//     },
+//   });
+//   tl.to(".line h2", {
+//     animationName: "Anime",
+//     opacity: 1,
+//   });
+//   tl.to("#loader", {
+//     opacity: 0,
+//     duration: 0.2,
+//     delay: 0,
+//   });
+//   tl.from("#page1", {
+//     delay: 0.2,
+//     y: 1600,
+//     opacity: 0,
+//     ease: Power4,
+//     duration: 0.5,
+//   });
+//   tl.to("#loader", {
+//     display: "none",
+//   });
+//   tl.from("#nav", {
+//     opacity: 0,
+//   });
+//   tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1 ", {
+//     y: 120,
+//     stagger: 0.2,
+//   });
+// }
+// loadingAnimation();
 
 function cursorAnimation() {
   document.addEventListener("mousemove", function (dets) {
